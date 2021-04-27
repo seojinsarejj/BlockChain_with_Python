bb3fac95ac79f46a8faf97cfc562d6a1e1e80569
seojinsarejj <s19009@naver.com>
2021-04-27 14:18:12 +0900

    [CREATE] Create BlockChain

---
+++ src/blockchain.py
@@ -0,0 +1,48 @@
+import hashlib
+impot json
+from time import time
+
+class Blockchain(object):
+
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        self.new_block(previous_hash=1,proof=1 00)
+
+
+    def new_block(self, proof, previous_hash=None):
+        
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transaction': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+
+    @staticmethod
+    def hash(block):
+        
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
